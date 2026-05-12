0c1fdd1df0aa6a5a6dc5ac8af84eea1028e22026
SaulSivincha <ssivincham@unsa.edu.pe>
2026-05-12 12:00:04 -0500

    Variables

--- 2.Variables.c
+++ 2.Variables.c
@@ -1,5 +1,12 @@
 #include <stdio.h>
 
 int main() {
+    int edad = 20;
+    char nombre[] = "Saul";
+    double estatura = 1.71;
+
+    printf("%d\n", edad);
+    printf("%.2f\n", estatura);
+    printf("%s\n", nombre);
     return 0;
 }
