fbf47e74b293dd50d1101a904e85a31453bfb49b
SaulSivincha <ssivincham@unsa.edu.pe>
2026-07-01 23:04:01 -0500

    estructura ownesrhitp

---
+++ 01.-Punteros/4.Memoria Dinamica Basica/5.Ownership.c
@@ -0,0 +1,49 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+float *crearNotas(int cantidad) {
+    // reservar memoria con malloc
+
+    // verificar si malloc fallo
+
+    // devolver el puntero
+}
+
+void llenarNotas(float *notas, int cantidad) {
+    // pedir cada nota y guardarla en el arreglo
+}
+
+void imprimirNotas(float *notas, int cantidad) {
+    // imprimir todas las notas
+}
+
+float calcularPromedio(float *notas, int cantidad) {
+    // sumar todas las notas
+
+    // devolver promedio
+}
+
+int main(void) {
+    int cantidad = 0;
+
+    printf("Cantidad de estudiantes: ");
+    scanf("%d", &cantidad);
+
+    // validar cantidad
+
+    // crear arreglo llamando a crearNotas
+
+    // verificar si devolvio NULL
+
+    // llenar notas
+
+    // imprimir notas
+
+    // calcular promedio
+
+    // mostrar promedio
+
+    // liberar memoria
+
+    return 0;
+}
