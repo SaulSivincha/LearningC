64f98ec450a96d07c09dd55fe699e100fa73e1b5
SaulSivincha <ssivincham@unsa.edu.pe>
2026-07-14 10:50:32 -0500

    correcion

--- 05-Hilos-Pthreads/6.Hilos-y-forks.c
+++ 05-Hilos-Pthreads/6.Hilos-y-forks.c
@@ -1,54 +1,52 @@
 #include <stdio.h>
-#include <stdlib.h>
 #include <pthread.h>
 
-struct typedef {
+typedef struct {
     int inicio;
     int final;
     int hilo;
 } Rango;
 
-void *sumadorRango (void *arg) {
-    int *rango = (int *)arg;
-    for (i = rango->inicio; i <= rango->final; i++) {
-        printf("Hilo %d imprimiendo: %d\n", rango->hilo, i);
+void *sumadorRango(void *arg) {
+    Rango *rango = (Rango *)arg;
+
+    for (int i = rango->inicio; i <= rango->final; i++) {
+        printf(
+            "Hilo %d imprimiendo: %d\n",
+            rango->hilo,
+            i
+        );
     }
+
     return NULL;
 }
 
-int main () {
-
-    Rango rango[3] = {
+int main(void) {
+    Rango rangos[3] = {
         {0, 33, 1},
         {34, 67, 2},
         {68, 100, 3}
     };
 
-    pthread_t = hilo1;
-    hilo1 = pthread_create(&hilo1, NULL, sumadorRango, &rango[0]);
-    if (hilo1 != 0) {
-        printf("Error");
-        return 1;
-    }   
-    pthread_join(hilo1, NULL);
+    pthread_t hilos[3];
 
+    for (int i = 0; i < 3; i++) {
+        int estado = pthread_create(
+            &hilos[i],
+            NULL,
+            sumadorRango,
+            &rangos[i]
+        );
 
-    pthread_t = hilo2;
-    hilo2 = pthread_create(&hilo2, NULL, sumadorRango, &rango[1]);
-    if (hilo2 != 0) {
-        printf("Error");
-        return 1;
-    }   
-    pthread_join(hilo2, NULL);
+        if (estado != 0) {
+            printf("Error al crear el hilo %d\n", i + 1);
+            return 1;
+        }
+    }
 
-    pthread_t = hilo3;
-    hilo3 = pthread_create(&hilo3, NULL, sumadorRango, &rango[2]);
-    if (hilo3 != 0) {
-        printf("Error");
-        return 1;
-    }   
-    pthread_join(hilo3, NULL);
+    for (int i = 0; i < 3; i++) {
+        pthread_join(hilos[i], NULL);
+    }
 
     return 0;
-
 }
